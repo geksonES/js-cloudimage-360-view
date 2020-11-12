616fd4439bbaddb8c66b121c66ef9df40a6d5d17
Евгений Соколов <sokolov99workemail@gmail.com>
2020-11-12 11:45:59 +0200

    added isNoViewers check

--- src/index.js
+++ src/index.js
@@ -37,6 +37,7 @@ window.CI360 = window.CI360 || {};
 window.CI360.init = init;
 window.CI360.destroy = destroy;
 window.CI360.getActiveIndexByID = getActiveIndexByID;
+window.CI360.isNoViewers = isNoViewers;
 
 if (!window.CI360.notInitOnLoad) {
   init();
